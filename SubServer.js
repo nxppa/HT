54958ddabc2859d74cdc18b3386187c67c1bc816
Nappa <079nappa173@gmail.com>
2024-11-17 23:05:31 +1100

    Update SubServer.js

--- SubServer.js
+++ SubServer.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 80
 const BackupIp = "162.243.67.114"
 
-const server = app.listen(PORT, BackupIp, function (err) {
+const server = app.listen(port, BackupIp, function (err) {
     if (err) console.log(err);
     console.log("Server listening on PORT", PORT);
   });
