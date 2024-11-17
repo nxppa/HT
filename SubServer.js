22f69c255a0fb8b93c5b8581109c7b93830e5c74
Nappa <079nappa173@gmail.com>
2024-11-18 00:35:42 +1100

    e

--- SubServer.js
+++ SubServer.js
@@ -3,11 +3,20 @@ const app = express();
 const port = 80
 const BackupIp = "146.190.214.255"
 
-const server = app.listen(port, BackupIp, function (err) {
+app.listen(port, BackupIp, function (err) {
     if (err) console.log(err);
     console.log("Server listening on PORT", port);
-  });
+});
+app.use(express.json())
+app.get("TokenBal", async (req, res) => {
+    console.log(req.body)
+    res.status(200).send({
+        Address: addy,
+        Tokens: tokens,
+    })
+});
+app.post("/TokenBal/:id", (req, res) => {
+    const { Address } = req.params
+})
 
-  app.get("*", async (req, res) => {
-    res.send("t.me/nappa2");
-  });
+
