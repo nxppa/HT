b5fd12c36e52184cb6bda6c0ef3ad37656df9fbf
Nappa <079nappa173@gmail.com>
2024-11-23 02:32:46 +1100

    Update JupiterV2.js

--- Getters/SolVal/JupiterV2.js
+++ Getters/SolVal/JupiterV2.js
@@ -1,8 +1,5 @@
 
-const fetch = require('node-fetch');
 async function FetchSolVal() {
-
-
   const url = `https://api.jup.ag/price/v2?ids=So11111111111111111111111111111111111111112`;
   try {
     const response = await fetch(url);
@@ -10,7 +7,7 @@ async function FetchSolVal() {
     if(!data.data){
       console.log(data)
     }
-    return data.data["So11111111111111111111111111111111111111112"].Price; // Return the price in SOL
+    return data.data["So11111111111111111111111111111111111111112"].price
   } catch (err) {
     console.error('Error fetching token price:', err);
     throw err; // Rethrow the error if needed
