7d9ce149e60f12eae254d20f506cd6d41e95ada0
Nappa <079nappa173@gmail.com>
2024-12-01 17:50:00 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1015,7 +1015,6 @@ async function handleMessage(messageObj) {
     "clearwallets": "❌ Clear Wallets",
     "confirmation": "✔️ Yes",
     "walletgen": "🗝️ Generate Wallet",
-    "kill": "🔪 Kill bot"
   }
 
   if (!IDToName[chatId]) {
@@ -1328,8 +1327,6 @@ async function handleMessage(messageObj) {
       const msg = `💼 Wallet: \`\`\`${PubKey}\`\`\` \n 🗝️ Key: \`\`\`${PrivKey}\`\`\` ` //TODO make a rate limit for this
       sendMessage(chatId, msg, "MarkdownV2")
       return
-    case ActionTexts["kill"]:
-      process.exit(0)
     case ActionTexts["mybal"]:
       async function showbal() {
         sendMessage(chatId, `Getting balance for ${GetWalletEmbed("Wallet", MyWallet)}`, "MarkdownV2")
@@ -1373,7 +1370,6 @@ async function handleMessage(messageObj) {
     case ActionTexts["actions"]:
       const ActionOptions = [
         { text: ActionTexts["walletgen"] },
-        { text: ActionTexts["kill"] },
         { text: ActionTexts["back"] },
       ]
 
