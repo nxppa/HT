f0d235b0d7dd1702d2e2b3c155b8f2f53afb3339
Nappa <079nappa173@gmail.com>
2024-12-03 16:54:13 +1100

    Update GetTokens.js

--- Getters/TokenBalance/GetTokens.js
+++ Getters/TokenBalance/GetTokens.js
@@ -7,9 +7,10 @@ const SOLANA_RPC_ENDPOINTS = {
    //"publicnode": "https://solana-rpc.publicnode.com/",
    "quiknode-2": "https://virulent-few-dawn.solana-mainnet.quiknode.pro/272b003581d3e1ec81ab5ccf9f7a8008cb0453ec",
    "quiknode-1": "https://flashy-radial-needle.solana-mainnet.quiknode.pro/1f355b50797c678551df08ed13bb94295ebebfc7",
+   "helius": "https://mainnet.helius-rpc.com/?api-key=62867695-c3eb-46cb-b5bc-1953cf48659f",
    //"tracker": "https://rpc-mainnet.solanatracker.io/?api_key=81b71925-ca06-487c-ac6c-155d8a9e3cda",
    //"syndica": "https://solana-mainnet.api.syndica.io/api-key/4MPquh8r1sBddBwSk6bN3pEHWF241B15QjPVGM5NJCTaetdXSKWyKiGrbw2XtM6YLa6EnYUExb6c5Hras1ocYuUks3YvmtMKDNj",
-   //"helius": "https://mainnet.helius-rpc.com/?api-key=62867695-c3eb-46cb-b5bc-1953cf48659f",
+   //"ligma": "https://public.ligmanode.com",
 };
 
 
@@ -41,11 +42,12 @@ function deepEqual(obj1, obj2) {
 
     return true;
 }
-
+const connections = {};
+for (const [name, rpc] of Object.entries(SOLANA_RPC_ENDPOINTS)) {
+    connections[name] = new Connection(rpc, { commitment: 'confirmed' });
+}
 async function fetchTokensFromEndpoint(rpc, address, programId) {
-    const connection = new Connection(rpc, {
-        commitment: 'confirmed',
-    });
+    const connection = connections[rpc]
 
     const response = await connection.getParsedTokenAccountsByOwner(address, { programId });
     const tokens = {};
