fdb0808f982d0b6d10b57b054313e9165f03653b
Nappa <079nappa173@gmail.com>
2024-12-03 16:46:40 +1100

    Update Transaction.js

--- Transaction.js
+++ Transaction.js
@@ -1,11 +1,9 @@
 const {Swap} = require("./Operations/PumpPortal.js")
-function GetTime(raw) {
-    const now = new Date();
-    let time = raw 
-        ? `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()},${now.getMilliseconds()}` 
-        : `[${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}.${now.getMilliseconds()}]`;
 
-    return time;
+async function main(){
+    Swap("5aZDm6LbgbmnBSQiKtSbSoRRMHn3wkPB8cUD5G4BvAcA", 1, 40, 0.0001, )
+
 }
-console.log(GetTime())
-Swap("FbwCKEyyEhCwfbde3Wzg7CFSSzfbwsa6HKLuNLjkpump", 1, 40, 0.0001, "buy")
+
+
+main()
