8ed97806c670225c05c38b21b5bbabf1b0f4c6f8
Nappa <079nappa173@gmail.com>
2024-12-04 18:58:36 +1100

    Update main.js

--- main.js
+++ main.js
@@ -639,14 +639,16 @@ async function handleSwap(Mint, InpAmount, transactionType) {
 
 const SOLANA_RPC_ENDPOINTS = [ //TODO make it so that this is a universal variable
   //"https://api.mainnet-beta.solana.com",
-  //"https://mainnet.helius-rpc.com/?api-key=62867695-c3eb-46cb-b5bc-1953cf48659f",
-  "https://flashy-radial-needle.solana-mainnet.quiknode.pro/1f355b50797c678551df08ed13bb94295ebebfc7",
   //"https://rpc-mainnet.solanatracker.io/?api_key=81b71925-ca06-487c-ac6c-155d8a9e3cda",
   //"https://solana-mainnet.core.chainstack.com/155d8d316c41d2ab16e07ee9190e409c",
   //"https://solana-rpc.publicnode.com/",
-  //"https://solana-mainnet.api.syndica.io/api-key/4MPquh8r1sBddBwSk6bN3pEHWF241B15QjPVGM5NJCTaetdXSKWyKiGrbw2XtM6YLa6EnYUExb6c5Hras1ocYuUks3YvmtMKDNj",
+  
+  
+  "https://mainnet.helius-rpc.com/?api-key=62867695-c3eb-46cb-b5bc-1953cf48659f",
+  "https://flashy-radial-needle.solana-mainnet.quiknode.pro/1f355b50797c678551df08ed13bb94295ebebfc7",
   "https://virulent-few-dawn.solana-mainnet.quiknode.pro/272b003581d3e1ec81ab5ccf9f7a8008cb0453ec",
   "https://public.ligmanode.com",
+  //"https://solana-mainnet.api.syndica.io/api-key/4MPquh8r1sBddBwSk6bN3pEHWF241B15QjPVGM5NJCTaetdXSKWyKiGrbw2XtM6YLa6EnYUExb6c5Hras1ocYuUks3YvmtMKDNj",
 ];
 
 const connections = {};
