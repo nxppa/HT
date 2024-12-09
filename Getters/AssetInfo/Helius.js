5cfc6ffc2c429e080f47bdee0ae04272900d70d5
Nappa <079nappa173@gmail.com>
2024-12-09 21:29:29 +1100

    Update Helius.js

--- Getters/AssetInfo/Helius.js
+++ Getters/AssetInfo/Helius.js
@@ -27,7 +27,13 @@ async function getAsset(assetId) {
         }
 
         const data = await response.json();
-
+        console.log(data.result.files)
+        const Output = {
+            "Name": data.result.content.metadata.name,
+            "Description": data.result.content.metadata.description,
+            "Symbol": data.result.content.metadata.symbol,
+            //"Image": data.result.content.files.uri
+        }
         return data.result.files;
     } catch (error) {
         console.error('Error fetching asset data:', error);
