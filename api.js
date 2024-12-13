60763be63bae135c42427ccb4d46f69e5b381991
Nappa <079nappa173@gmail.com>
2024-12-14 01:57:53 +1100

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,10 @@ const { AnalyseAccount } = require('./Getters/AccountAnalysis/AnalyseAccount');
 const app = express();
 const port = 8080;
 const BackupIp = "142.93.123.245";
+const ValidKeys = {
+    "qwerty123": "Nappa"
 
+}
 
 app.listen(port, BackupIp, function (err) {
     if (err) console.log(err);
@@ -12,6 +15,14 @@ app.listen(port, BackupIp, function (err) {
 app.use(express.json());
 
 app.get("/api/tools/scanner", async (req, res) => {
+    const ApiKey = req.query.key
+    const KeyOwner = ValidKeys[ApiKey]
+    if (!ApiKey){
+        return  res.status(400).send({ error: "API key needed" });
+    }
+    if (!KeyOwner){
+        return  res.status(401).send({ error: "Invalid API key" });
+    }
     const AccountToScan = req.query.account
 
     if (!AccountToScan) {
@@ -23,5 +34,17 @@ app.get("/api/tools/scanner", async (req, res) => {
     }
     res.status(200).send(Response);
 });
+app.get("/api/tools/generateWallet", async (req, res) => {
+
+    if (!AccountToScan) {
+        return res.status(400).send({ error: "Account parameter is required" });
+    }
+    const Response = await AnalyseAccount(AccountToScan)
+    if (typeof(Response) == "string") {
+        return res.status(404).send({ error: Response });
+    }
+    res.status(200).send(Response);
+});
+
 
 //TODO make sanity check; only allow requests from whitelisted ips
