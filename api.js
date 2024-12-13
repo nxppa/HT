92787615e3a7a86aaaa0c5b3e6eab3299b8a6d45
Nappa <079nappa173@gmail.com>
2024-12-14 06:02:57 +1100

    updates

--- api.js
+++ api.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const fs = require('fs');
 const { AnalyseAccount } = require('./Getters/AccountAnalysis/AnalyseAccount');
 const { Connection, PublicKey, clusterApiUrl, Keypair, VersionedTransaction, Message } = require('@solana/web3.js');
 const { publicKey } = require('@raydium-io/raydium-sdk');
+const bcrypt = require('bcrypt');
 const app = express();
 const MaxWallets = 100
 const port = 8080; //TODO make env files
 const BackupIp = "142.93.123.245";
 const ValidKeys = {
-    "qwerty123": "Nappa"
+    "34a75bef08004f789e548e709171c8822af6aec25b32260a169d2ac384746ecf8a01b6eb0ff56653d1e4fb5361e2f10495edea7ef56ef8d42884fea2b061e605": "Nappa"
 }
 const SOLANA_RPC_ENDPOINT = "https://public.ligmanode.com" //TODO MAYBE make it use multiple endpoints 
 const connection = new Connection(SOLANA_RPC_ENDPOINT, {
@@ -20,7 +22,32 @@ app.listen(port, BackupIp, function (err) {
 });
 app.use(express.json());
 
+
+async function generateHash(password) {
+    const saltRounds = 10; // The higher the number, the more secure (but slower) the hash generation
+    try {
+        // Generate the hash
+        const hash = await bcrypt.hash(password, saltRounds);
+        console.log('Hashed Password:', hash);
+        return hash;
+    } catch (error) {
+        console.error('Error generating hash:', error);
+    }
+}
+
+function Encode(Key){
+
+}
+function Decode(Key){
+    
+}
+
 function KeyCheck(res, key){
+    const ValidKeys = fs.readFileSync("./db/Passes.json");
+
+
+
+
     if (!key){
         res.status(400).send({ error: "API key needed" });
         return  false
@@ -69,6 +96,27 @@ app.get("/api/tools/getBalance", async (req, res) => {
     Response.Balance = Balance
     res.status(200).send(Response);
 });
+
+
+app.get("/authenticate", async (req, res) => { 
+
+    const { password } = req.body;
+    try {
+        // Compare provided password with the stored hash
+        const isMatch = await bcrypt.compare(password, userDatabase.passwordHash);
+
+        if (isMatch) {
+            res.json({ success: true, message: 'Authentication successful!' });
+        } else {
+            res.status(401).json({ success: false, message: 'Invalid password' });
+        }
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Server error' });
+    }
+});
+
+
+
 
 app.get("/api/tools/generateWallets", async (req, res) => { 
     if (!KeyCheck(res, req.query.key)) return;
