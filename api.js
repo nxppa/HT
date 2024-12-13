a56e5d651f2c2525e083206b6a6f512969d8d147
Nappa <079nappa173@gmail.com>
2024-12-14 02:00:45 +1100

    Update api.js

--- api.js
+++ api.js
@@ -14,20 +14,20 @@ app.listen(port, BackupIp, function (err) {
 });
 app.use(express.json());
 
-function KeyCheck(res){
+function KeyCheck(res, key){
+    const KeyOwner = ValidKeys[key]
     if (!ApiKey){
         return  res.status(400).send({ error: "API key needed" });
     }
     if (!KeyOwner){
         return  res.status(401).send({ error: "Invalid API key" });
     }
-
 }
 
 app.get("/api/tools/scanner", async (req, res) => {
     const ApiKey = req.query.key
-    const KeyOwner = ValidKeys[ApiKey]
-    KeyCheck(res)
+    KeyCheck(res, ApiKey)
+
     const AccountToScan = req.query.account
 
     if (!AccountToScan) {
