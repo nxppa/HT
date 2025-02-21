2c02e64dc8f59b9a078ad63717b3ec09c232eca5
Nappa <079nappa173@gmail.com>
2025-02-22 09:30:46 +1100

    u

--- Getters/TokenBalance/MainServer.js
+++
@@ -1,6 +0,0 @@
-const GetTokens = require("./GetTokens")
-async function MainGetTokens(Address) {
-    console.log("using main", Address)
-    return GetTokens(Address)
-}
-module.exports = MainGetTokens
